226efb450dc0f263f8f828ad2a4a9e5caeeee79b
EC2 Default User <ec2-user@ip-172-31-23-72.ec2.internal>
2022-01-15 07:04:38 +0000

    schema.rb修正

--- db/schema.rb
+++ db/schema.rb
@@ -12,18 +12,6 @@
 
 ActiveRecord::Schema.define(version: 2022_01_15_045224) do
 
-  create_table "items", force: :cascade do |t|
-    t.string "name"
-    t.boolean "is_active", default: true, null: false
-    t.text "item_introduction"
-    t.integer "price"
-    t.integer "image_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-ActiveRecord::Schema.define(version: 2022_01_15_043226) do
-
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -54,6 +42,16 @@ ActiveRecord::Schema.define(version: 2022_01_15_043226) do
     t.boolean "is_registered", default: true
     t.index ["email"], name: "index_customers_on_email", unique: true
     t.index ["reset_password_token"], name: "index_customers_on_reset_password_token", unique: true
+  end
+
+  create_table "items", force: :cascade do |t|
+    t.string "name"
+    t.boolean "is_active", default: true, null: false
+    t.text "item_introduction"
+    t.integer "price"
+    t.integer "image_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
