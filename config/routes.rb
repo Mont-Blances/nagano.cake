392a769d285ee45223b7a8223f9b7101500ff994
pokori20 <93557699+pokori20@users.noreply.github.com>
2022-01-16 06:39:02 +0000

    create genre view controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
+  namespace :admin do
+  resources :genres, only: [:create, :edit, :index, :update]
+  end
   devise_for :admins
   devise_for :customers
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+#   has_many items
 end
