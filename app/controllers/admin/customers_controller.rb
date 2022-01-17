16c3d102b40fd5d367d78899ad9598b1a5e444fa
EC2 Default User <ec2-user@ip-172-31-42-65.ec2.internal>
2022-01-17 12:57:51 +0000

    cutomer関係機能追加

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -1,2 +1,29 @@
 class Admin::CustomersController < ApplicationController
+  def index
+    @customers = Customer.page(params[:page]).per(10)
+  end
+
+  def show
+    @customer = Customer.find(params[:id])
+  end
+
+  def edit
+    @customer = Customer.find(params[:id])
+  end
+
+  def update
+    @customer = Customer.find(params[:id])
+    if @customer.update(customer_params)
+      redirect_to admin_customer_path(@customer), notice: "登録情報を更新しました。"
+    else
+      render "edit"
+    end
+  end
+
+  private
+
+  def customer_params
+    params.require(:customer).permit(:family_name, :first_name, :family_name_kana, :first_name_kana, :postcode, :adress, :tel, :is_registered)
+  end
+
 end
