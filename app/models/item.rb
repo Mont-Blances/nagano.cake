15537cb5ca4659aa0af31bfb19b3124527339ec0
pokori20 <93557699+pokori20@users.noreply.github.com>
2022-01-17 10:14:14 +0000

    :sparkles: create public items
    public/itemsのindexとshow画面作成cart.rbとitem.rbにリレーション記

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,7 @@
 class Item < ApplicationRecord
   attachment :image
   belongs_to :genre
+  has_many :cart_items
     with_options presence: true do
     validates :name
     validates :item_introduction
