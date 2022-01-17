1ec53ac118e1d70f657c7a7779f93547f8345e8b
pokori20 <93557699+pokori20@users.noreply.github.com>
2022-01-17 11:51:33 +0000

    :construction: create cart_item
    cart_itemのindex作成とcontroller記述

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,4 +1,27 @@
 class Public::CartItemsController < ApplicationController
+  
   def index
+    @cart_items = CartItem.all
+  end
+
+  def create
+    @cart_item = CartItem.new(params)
+    @cart_item.save
+    redirect_back(fallback_location: root_path)
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  def destroy_all
+  end
+
+  private
+
+  def cart_item_params
+    params.require(:cart_item).permit(:quantity)
   end
 end
