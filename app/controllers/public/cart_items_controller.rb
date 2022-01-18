f065fb85c2261e609222b6824147ab508d960c3f
pokori20 <93557699+pokori20@users.noreply.github.com>
2022-01-18 10:45:39 +0000

    :sparkles: cart_items index
    カートindex　カートを空にする　個数選択機能実装

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,11 +1,22 @@
 class Public::CartItemsController < ApplicationController
-  
+
   def index
-    @cart_items = CartItem.all
+    @cart_items = current_customer.cart_items.all
+
   end
 
   def create
-    @cart_item = CartItem.new(params)
+    # binding.pry
+    @cart_item = current_customer.cart_items.new(cart_item_params)
+
+    @cart_items = current_customer.cart_items.all
+    @cart_items.each do |cart_item|
+      if cart_item.item_id == @cart_item.item_id
+      new_quantity = cart_item.quantity + @cart_item.quantity
+      cart_item.update_attribute(:quantity, new_quantity)
+      @cart_item.delete
+      end
+    end
     @cart_item.save
     redirect_back(fallback_location: root_path)
   end
@@ -14,14 +25,21 @@ class Public::CartItemsController < ApplicationController
   end
 
   def destroy
+    @cart_item = current_customer.cart_items.find(params[:id])
+    @cart_item.destroy
+    redirect_back(fallback_location: root_path)
   end
 
   def destroy_all
+    # cart_items.where(customer_id: current_customer.id).destroy_all
+    @cart_items = current_customer.cart_items
+    @cart_items.destroy_all
+    redirect_back(fallback_location: root_path)
   end
 
   private
 
   def cart_item_params
-    params.require(:cart_item).permit(:quantity)
+    params.require(:cart_item).permit(:quantity, :item_id, :customer_id)
   end
 end
