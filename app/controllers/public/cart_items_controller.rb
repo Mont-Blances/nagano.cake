b630cee054332028a5e2b41aa438987a2d2707eb
EC2 Default User <ec2-user@ip-172-31-42-65.ec2.internal>
2022-01-20 07:55:54 +0000

    エラー修正

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -23,8 +23,8 @@ class Public::CartItemsController < ApplicationController
 
   def update
     @cart_item = current_customer.cart_items.find(params[:id])
-    @cart_item.update(cart_item_update_params)
-    redirect_back(fallback_location: root_path)
+    @cart_item.update(cart_item_params)
+    redirect_to cart_items_path, notice: '数量を変更しました'
   end
 
   def destroy
@@ -46,8 +46,5 @@ class Public::CartItemsController < ApplicationController
     params.require(:cart_item).permit(:quantity, :item_id, :customer_id)
   end
 
-  def cart_item_update_params
-    params.require(:cart_item).permit(:item_id,:quantity)
-  end
 
 end
