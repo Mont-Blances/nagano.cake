21cdce033b369fc5740fc0e1abb8085ba9d476be
EC2 Default User <ec2-user@ip-172-31-42-65.ec2.internal>
2022-01-21 12:43:55 +0000

    view追加

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,14 +1,18 @@
 class Order < ApplicationRecord
   enum payment_method:{credit: 0,bank:1}
   enum order_status:{wait: 0, confirmation: 1, making: 2, preparation: 3, finish: 4}
-  
+
    belongs_to :customer
    has_many :order_detail, dependent: :destroy
-   
+
   validates :customer_id, presence: true
   # validates :postcord
   validates :address, presence: true
   # validates :destination
   validates :payment_method, presence: true
 #   validates :billing_amount, presence: true
+
+  def order_items_billing_amount
+    (billing_amount - 800).round
+  end
 end
--- app/models/order_detail.rb
+++ app/models/order_detail.rb
@@ -1,6 +1,7 @@
 class OrderDetail < ApplicationRecord
   enum production_status:{before: 0,wait:1,make:2,finish:3}
-  
+
 belongs_to :order
-  
+belongs_to :item
+
 end
