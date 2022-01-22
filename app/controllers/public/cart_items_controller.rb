c02727512ce57b12a444f4273a8049f93c915ba3
EC2 Default User <ec2-user@ip-172-31-42-65.ec2.internal>
2022-01-22 05:37:51 +0000

    カート機能修正

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -5,10 +5,10 @@ class Public::CartItemsController < ApplicationController
   end
 
   def create
-    @cart_item = current_customer.cart_items.find_by(item_id: params[:item_id])
+    @cart_item = current_customer.cart_items.find_by(item_id: params[:cart_item][:item_id])
 
     if @cart_item.present?
-      @cart_item.quantity += params[:quantity].to_i
+      @cart_item.quantity += params[:cart_item][:quantity].to_i
     else
       @cart_item = current_customer.cart_items.new(cart_item_params)
     end
@@ -16,7 +16,9 @@ class Public::CartItemsController < ApplicationController
     if @cart_item.quantity.nil?
       flash[:alert] = '数量を選択して下さい'
       redirect_to request.referer
-    elsif  @cart_item.save
+    end
+
+    if @cart_item.save
       redirect_to cart_items_path, notice: '商品を追加しました'
     else
       flash[:alert] = '商品を追加できませんでした'
