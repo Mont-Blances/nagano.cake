da0a5173ecaba8b8e87ab0cf412841f817dbb2c7
EC2 Default User <ec2-user@ip-172-31-42-65.ec2.internal>
2022-01-23 12:50:49 +0000

    ジャンル修正

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -1,17 +1,17 @@
 class Admin::GenresController < ApplicationController
+  before_action :authenticate_admin!
+  
   def index
     @genres = Genre.all
     @genre = Genre.new
   end
 
   def create
-    @genres = Genre.all
     @genre = Genre.new(genre_params)
+    @genres = Genre.all
     if @genre.save
-      flash.now[:notice] = "ジャンルを追加しました"
       redirect_to admin_genres_path
     else
-      flash.now[:alert] = "入力内容をご確認ください"
       render :index
     end
   end
@@ -23,17 +23,14 @@ class Admin::GenresController < ApplicationController
   def update
     @genre = Genre.find(params[:id])
     if @genre.update(genre_params)
-      flash.now[:notice] = "ジャンルを変更しました"
-    　redirect_to action: :index
+      redirect_to admin_genres_path
     else
-      flash.now[:alert] = "入力内容を確認してください。"
       render :edit
     end
   end
 
   private
-
   def genre_params
-    params.require(:genre).permit(:name)
+  params.require(:genre).permit(:name)
   end
 end
