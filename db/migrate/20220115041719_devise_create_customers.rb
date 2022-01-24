49d59c2f7f916ab6521858896c43ef6125741b5c
pokori20 <93557699+pokori20@users.noreply.github.com>
2022-01-24 05:15:28 +0000

    :bug: order new
    注文時の配属先のデータ取得を修正

--- db/migrate/20220115041719_devise_create_customers.rb
+++ db/migrate/20220115041719_devise_create_customers.rb
@@ -39,6 +39,7 @@ class DeviseCreateCustomers < ActiveRecord::Migration[5.2]
       t.string :postcode
       t.string :address
       t.string :tel
+      t.boolean :is_registered ,default: true
 
 
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_15_081543) do
+ActiveRecord::Schema.define(version: 2022_01_17_054916) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -45,6 +45,7 @@ ActiveRecord::Schema.define(version: 2022_01_15_081543) do
     t.string "postcode"
     t.string "address"
     t.string "tel"
+    t.boolean "is_registered", default: true
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_customers_on_email", unique: true
@@ -71,10 +72,10 @@ ActiveRecord::Schema.define(version: 2022_01_15_081543) do
     t.boolean "is_active", default: true, null: false
     t.text "item_introduction"
     t.integer "price"
-    t.string "image_id"
-    t.integer "genre_id"
+    t.integer "image_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "genre_id"
   end
 
   create_table "order_details", force: :cascade do |t|
