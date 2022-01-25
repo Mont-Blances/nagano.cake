44c7f9e132bba23a07be48340c35a28e0c1377d2
pokori20 <93557699+pokori20@users.noreply.github.com>
2022-01-25 05:37:14 +0000

    :recycle: リファクタリング
    コードリファクタリング

--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -5,7 +5,7 @@ class CartItem < ApplicationRecord
   validates :quantity, numericality: {greater_than_or_equal_to:1,less_than_or_equal_to:10, message: '1回の注文は10個までです。' }
   
   def sum_taxin_price # 実際に作成したサイトは税金も算出していたのでメソッドを記載していました
-    (item.price*quantity*1.1).round
+    (item.price*quantity*1.10).round
   end
 
 
